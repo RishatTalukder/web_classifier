92fb24a80c576db73c460d9cdd4946861e19b877
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2026-09-26 12:06:54 +0600

    Convert model from Keras LayersModel to TensorFlow.js GraphModel format

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,34 @@ import * as tf from '@tensorflow/tfjs'
 
 const App = () => {
   const [loading, setLoading] = React.useState(true)
-  const [model, setModel] = React.useState<tf.LayersModel | null>(null)
+  const [model, setModel] = React.useState<tf.GraphModel | null>(null)
 
-  useEffect(()=>{
-    const loadModel = async ()=>{
-      try{
-        const loadedModel = await tf.loadLayersModel('model/model.json')
-        
-        tf.tidy(()=>{
-          loadedModel.predict(tf.zeros([1,28,28,1]))
+  useEffect(() => {
+    const loadModel = async () => {
+      try {
+        const loadedModel = await tf.loadGraphModel('model/model.json')
+
+        tf.tidy(() => {
+          loadedModel.predict(tf.zeros([1, 28, 28, 1]))
         })
 
         setModel(loadedModel)
         setLoading(false)
+
         console.log('model loaded')
-      } catch(e) {
+      } catch (e) {
         console.error(e)
       }
     }
+
     loadModel()
   }, [])
 
-
   return (
     <div>
-      <h1>
-        Life is a Race
-      </h1>
-      {
-        loading
-      }
+      <h1>Life is a Race</h1>
+
+      {loading && <p>Loading model...</p>}
     </div>
   )
 }
